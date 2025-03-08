bc3f71d8a74a3552dcffa42eb116e97f93afac73
Prashant <prashantcoder70@gmail.com>
2025-03-08 22:45:58 +0530

    db icres

--- Utils/db.js
+++ Utils/db.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
-const DbConnection = ()=>
-{
-    mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>
-    {
-        console.log("Db is Connected");
-        
-    }).catch((err)=>
-    {
-            console.log(`db error ${err}`);
-            
+const DbConnection = () => {
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
     })
-}
+    .then(() => {
+      console.log("Db is Connected");
+    })
+    .catch((err) => {
+      console.log(`db error ${err}`);
+    });
+};
 export default DbConnection;
