e3d87b6a7509919a5abdc6a2ebbb1c1bceef582d
Prashant <prashantcoder70@gmail.com>
2025-03-08 22:24:57 +0530

    fixedd port remove

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/",(req,res)=>
 
 DbConnection();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
